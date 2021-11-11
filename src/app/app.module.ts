cb9c69683b5117be147226cdc2134a47bf758585
Brunno Khouri <75075067+BrunnoKhouri@users.noreply.github.com>
2021-11-11 02:19:15 -0300

    add-firstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HomeComponent } from './layout/home/home.component';
+import { BannerComponent } from './layout/banner/banner.component';
+import { CardComponent } from './layout/card/card.component';
+import { CarouselComponent } from './layout/carousel/carousel.component';
+import { GuestCardComponent } from './layout/guest-card/guest-card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    BannerComponent,
+    CardComponent,
+    CarouselComponent,
+    GuestCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AppRoutingModule,    
+    MatToolbarModule,
+    MatButtonModule,    
+    MatSliderModule,
+    MatCardModule,
+    MatIconModule,
+    MatChipsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
